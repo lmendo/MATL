4bfb4ecd218b1c6c9dbc875676caa775a0588e3a
Luis Mendo <lmendo@gmail.com>
2016-01-18 13:46:05 +0100

    Avoid unwanted text from vpa, second try

--- compatibility/vpa_comp.m
+++ compatibility/vpa_comp.m
@@ -1,7 +1,7 @@
 function y = vpa(varargin);
 % vpa in Octave produces a lot of unwanted displayed text. It's directly displayed, not part of the
-% function  output. So we evaluate the function to get rid of the text.
-% `builtin` serves to do the evaluation.
+% function  output. We solve that with PAGER. It may produce an error, but
+% at least it doesn't show in stdout.
 %   After converting to char, the result is different from Matlab. For
 % example, char(vpa(pi^pi)) produces the string
 %     'Float(''36.462159607207901501624291995540261'', prec=32)''
@@ -13,5 +13,10 @@ function y = vpa(varargin);
 % Both issues seem to be resolved by `pretty`:
 % `pretty(vpa('-3.4'))` gives the string
 %     '-3.4000000000000000000000000000000'
+if isunix % fix only for unix
+   PAGER('/dev/null','local');
+   page_screen_output(1, 'local');
+   page_output_immediately(1, 'local');
+end
 y = pretty(builtin('vpa', varargin{:}));
 end
