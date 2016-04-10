00e6a769e4e6199e8fa3e72d59cde2c4147f381d
Luis Mendo <lmendo@gmail.com>
2016-04-11 00:14:03 +0200

    Several improvements; see description
    * Improved `Xd` (`spdiags`): now it allows vector input with two outputs
    (Matlab and Octave don't)
    
    * `u` in stable mode now produces up to three outputs
    
    * `u` with numeric cell array now allows sorted and stable modes
    
    * Corrected a bug in the pad values used by `e` (`reshape`). Thanks,
    @suever! Also, this function with character input now pads with
    character 0
    
    * Octave compatibility. `Ym` (`mean`) and ' Zs` (`std`) now allow char
    input
    
    * Some functions (see for example `u`) now allow a number instead of a
    string input flag
    
    * `Zn` now also computes the determinant of a square matrix

--- compatibility/unique_comp.m
+++ compatibility/unique_comp.m
@@ -1,27 +1,31 @@
 function varargout = unique(varargin)
-% - Adds support for 'stable' flag. Only one output
+% - Adds support for 'stable' flag, with all three outputs
 % - The first ocurrence is returned, as in new Matlab versions. Octave, like old
 % Matlab version, returns the last.
 % - Second and third outputs are column vectors, as in new Matlab versions. 
 if nargin>=2 && strcmp(varargin{end},'stable')
     if strcmp(varargin{2},'rows')
-        [a, ~, y] = builtin('unique', varargin{1}, 'rows', 'first');
+        [a, ~, x] = builtin('unique', varargin{1}, 'rows', 'first');
     else
-        [a, ~, y] = builtin('unique', varargin{1}, 'first');
+        [a, ~, x] = builtin('unique', varargin{1}, 'first');
     end
-    if ~isempty(y)
-        y = y(~any(triu(bsxfun(@eq, y, y.'),1)));
+    if ~isempty(x)
+        [~, ind] = max(bsxfun(@eq, x(:), (1:max(x)))); [y, ind2] = sort(ind(:));
+        [~, ind3] = sort(ind2); z =  ind3(x);
+        x = x(~any(triu(bsxfun(@eq, x, x.'),1)));
     end    
     if strcmp(varargin{2},'rows')
-        y = a(y,:);
+        x = a(x,:);
     else
-        y = a(y);
+        x = a(x);
     end
-    varargout{1} = y; clear y
+    varargout{1} = x;
+    if nargout>=2, varargout{2} = y; end
+    if nargout>=3, varargout{3} = z; end
 else
     if strcmp(varargin{end},'sorted'), varargin(end) = []; end
     varargout = cell(1,nargout);
-    if isequal(varargin{end},'first') || isequal(varargin{end},'first'), varargin{end+1} = 'first'; end
+    if ~isequal(varargin{end},'first'), varargin{end+1} = 'first'; end
     [varargout{:}] = builtin('unique', varargin{:});
 end
 if nargout>=2, varargout{2} = varargout{2}(:); end
