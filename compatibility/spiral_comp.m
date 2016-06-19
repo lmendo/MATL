18f8c4ffa3b6dc952bd8a38cb8cf3430764d2004
Luis Mendo <lmendo@gmail.com>
2016-06-19 20:10:29 +0200

    Added `YF`: exponents of prime factorization. Changed implementation of `spiral` for Octave

--- compatibility/spiral_comp.m
+++ compatibility/spiral_comp.m
@@ -1,10 +1,12 @@
-function s = spiral(n)
-% This function doesn't exist in Octave
-s = zeros(n,n); i = ceil(n/2); j = ceil(n/2);
-s(i,j) = 1; if n == 1, return, end
-k = 1; d = 1;
-for p = 1:n
-    q = 1:min(p,n-1); j = j+d*q; k = k+q; s(i,j) = k; if (p == n), return, end
-    j = j(p); k = k(p); i = i+d*q'; k = k+q'; s(i,j) = k; i = i(p); k = k(p); d = -d;
+function y = spiral(N)
+% Implementation of `spiral`. It's slow, because the matrix is grown
+ss = reshape([1:N-1; 2:N], 1, []);
+y = 1;
+for s = ss;
+    y = [y (y(1,end)+1:y(1,end)+s).'];
+    y = rot90(y);
 end
+y = rot90(y, -numel(ss));
 end
+
+
