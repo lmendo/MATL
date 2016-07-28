b9e36f76140ccfb0c1d398412aa2a0d91c9cdc74
Luis Mendo <lmendo@gmail.com>
2016-07-29 00:39:30 +0200

    `&` for `XS`. Octave compatibility for `YX` (`regexprep`)
    * Octave compatibility: `YX` (`regexeprep`) now allows fourth input
    equal to `1` in Octave
    
    * `&` has been defined as `FT#` for `XS` (`sortrows`)

--- compatibility/regexprep_comp.m
+++ compatibility/regexprep_comp.m
@@ -1,7 +1,12 @@
 function varargout = regexprep(varargin)
+% Octave does not support numeric fourth input. To partly address this, fourth
+% input equal to 1 is replaced by 'once', so that at least that one works.
 % If regular expression is invalid, instead of an error it gives empty outputs, 
 % like Matlab does. For that we use a try...catch block; if an error is found we
 % use an empty regexp to produce empty outputs
+if nargin >=4 && isequal(varargin{4}, 1)
+    varargin{4} = 'once';
+end
 nargout = max(nargout,1);
 try
     [varargout{1:nargout}] = builtin('regexprep', varargin{:});
