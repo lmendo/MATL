01884f6c50eea9aae02d4866967468b39ed9c82d
Luis Mendo <lmendo@gmail.com>
2016-08-11 17:55:36 +0200

    Octave compatibility: `Zr` (`randsample`) allows first input to be char, and it is then treated as the population to sample from

--- compatibility/randsample_comp.m
+++ compatibility/randsample_comp.m
@@ -1,7 +1,12 @@
 function varargout = randsample(varargin)
-% - Produces a column in Octave (instead of a row) when the first input is a scalar
-varargout{1} = builtin('randsample', varargin{:});
-if numel(varargin{1})==1
-    varargout{1} = varargout{1}(:);
+% - Produces a column, like Matlab does (instead of Octave, which produces a row), when the first input is a scalar
+% - Allows first input to be char. It is then treated as the population to sample from (not as a number)
+if ischar(varargin{1})
+    varargout{1} = varargin{1}(randsample(numel(varargin{1}), varargin{2}));
+else
+    varargout{1} = builtin('randsample', varargin{:});
+    if numel(varargin{1})==1
+        varargout{1} = varargout{1}(:);
+    end
 end
 end
