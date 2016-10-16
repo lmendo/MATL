1bd786f263a658ea0677acf9ab1dd809f2e5ef0d
Luis Mendo <lmendo@gmail.com>
2016-10-17 01:23:34 +0200

    Solved bug in `randsample_comp.m`
    For first input of type char it was ignoring inputs beyond the second

--- compatibility/randsample_comp.m
+++ compatibility/randsample_comp.m
@@ -2,7 +2,7 @@ function varargout = randsample(varargin)
 % - Produces a column, like Matlab does (instead of Octave, which produces a row), when the first input is a scalar
 % - Allows first input to be char. It is then treated as the population to sample from (not as a number)
 if ischar(varargin{1})
-    varargout{1} = varargin{1}(randsample(numel(varargin{1}), varargin{2}));
+    varargout{1} = varargin{1}(randsample(numel(varargin{1}), varargin{2:end}));
 else
     varargout{1} = builtin('randsample', varargin{:});
     if numel(varargin{1})==1
