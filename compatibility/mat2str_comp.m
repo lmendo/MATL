bf56e4706d295b69c5eb4d7a8e3fb69ca58a7091
Luis Mendo <lmendo@gmail.com>
2017-05-23 01:40:31 +0200

    `D` with 1 ouput replaces most input characters below 32 by space
    `D` with 1 output now replaces most input characters below 32 by space
    (similar to what happens with `0` outputs).
    
    Corrected mat2str_comp, so that it doesn't remove trailing spaces

--- compatibility/mat2str_comp.m
+++ compatibility/mat2str_comp.m
@@ -6,12 +6,12 @@ if ischar(varargin{1});
     if severalRows
         z = num2cell(varargin{1},2);
         z = regexprep(z, '''', '''''');
-        z = strcat('''', z, ''';');
+        z = strcat('''', z, ''';'); % We use strcat here because z is a cell array. Trailing spaces are correctly preserved
         z = horzcat(z{:});
         z = [ '[' z(1:end-1)  ']'];
     else
         z = regexprep(varargin{1}, '''', '''''');
-        z = strcat('''', z, '''');
+        z = horzcat('''', z, ''''); % We use horzcat here because z is a string, and trailing spaces would be incorrectly removed by strcat
     end
 else
     z = builtin('mat2str', varargin{:});
