6e8faf569c531cc5d81f2a987f8b152f4d0a57b9
lmendo <lmendo@gmail.com>
2021-06-25 12:56:44 +0200

    Patched `bwlabeln`

---
+++ compatibility/bwlabeln_comp.m
@@ -0,0 +1,11 @@
+function varargout = bwlabeln_comp(varargin)
+% bwlabeln(1), bwlabeln(8), bwlabeln(true) give an error on TIO. This patches that
+if numel(varargin{1})==1 && varargin{1} % patch
+    varargout{1} = 1;
+    if nargout>1
+        varargout{2} = 1;
+    end
+else % normal call to bwlabeln
+    [varargout{1:nargout}] = builtin('bwlabeln', varargin{:});
+end
+end
